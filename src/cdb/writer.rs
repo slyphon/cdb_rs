107e5e3d10bba491342b526e04052cc352e425f9
Jonathan D. Simms <jsimms@twitter.com>
2018-06-11 14:05:40 -0400

    checkpoint

--- src/cdb/writer.rs
+++ src/cdb/writer.rs
@@ -1,11 +1,17 @@
+#[allow(dead_code)]
+
 use bytes::{Bytes,BytesMut, BufMut};
 
-use std::error::Error;
+use std::error;
 use std::fs::File;
 use std::io;
 use std::io::{BufReader,BufWriter};
 use std::io::prelude::*;
+use std::num::ParseIntError;
 use std::str;
+use std::str::Utf8Error;
+use std::fmt;
+
 use super::KV;
 
 pub struct Writer { }
@@ -17,92 +23,159 @@ const COMMA: u8 = 0x2c; // ASCII ','
 const COLON: u8 = 0x3a; // ASCII ':'
 const DASH: u8  = 0x2a; // ASCII '-'
 const GT: u8    = 0x3e; // ASCII '>'
-
-pub enum Cause {
-    IOError(io::Error),
-    UTF8Error(::std::str::Utf8Error),
-    ParseError(::std::num::ParseIntError)
-}
+const NL: u8    = 0x0a; // ASCII '\n'
 
 #[derive(Debug)]
-pub struct WriterError {
-    desc: String,
-    underlying: Option<Cause>
+pub enum WriterError {
+    IOError(io::Error),
+    UTF8Error(::std::str::Utf8Error),
+    ParseError(ParseIntError)
 }
 
-impl Error for WriterError {
-    fn description(&self) -> &str { self.desc.as_ref() }
-
-    fn cause(&self) -> Option<&Error> {
-        unimplemented!()
+impl From<ParseIntError> for WriterError {
+    fn from(err: ParseIntError) -> WriterError {
+        WriterError::ParseError(err)
     }
 }
 
-fn parse_digits(buf: &[u8]) -> Result<usize, Error> {
+impl From<Utf8Error> for WriterError {
+    fn from(err: Utf8Error) -> WriterError {
+        WriterError::UTF8Error(err)
+    }
+}
+
+impl From<io::Error> for WriterError {
+    fn from(err: io::Error) -> WriterError {
+        WriterError::IOError(err)
+    }
+}
+
+impl error::Error for WriterError {
+    fn description(&self) -> &str {
+        match *self {
+            WriterError::IOError(ref err) => err.description(),
+            WriterError::UTF8Error(ref err) => err.description(),
+            WriterError::ParseError(ref err) => err.description(),
+        }
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        match *self {
+            WriterError::IOError(ref err) => Some(err),
+            WriterError::UTF8Error(ref err) => Some(err),
+            WriterError::ParseError(ref err) => Some(err),
+        }
+    }
+}
+
+impl fmt::Display for WriterError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            WriterError::IOError(ref err) => err.fmt(f),
+            WriterError::UTF8Error(ref err) => err.fmt(f),
+            WriterError::ParseError(ref err) => err.fmt(f),
+        }
+    }
+}
+
+struct Parser<R: Read> {
+    input: BufReader<R>
+}
+
+fn parse_digits(buf: &[u8]) -> Result<usize, WriterError> {
     str::from_utf8(&buf)
-        .map_err(|err| Error::UTF8Error(err))
+        .map_err(|err| WriterError::UTF8Error(err))
         .and_then(|str|
             str.parse::<usize>()
-                .map_err(|err| Error::ParseError(err))
+                .map_err(|err| WriterError::ParseError(err))
         )
 }
 
+const ARROW_BYTES: &[u8; 2] = b"->";
 
-// format: +1,3:a->xyz\n
-fn read_sizes<R: Read>(input: &mut BufReader<R>) -> Result<KVSizes, Error> {
-    let mut buf: Vec<u8> = Vec::new();
+impl<R: Read> Parser<R> {
+    // format: +1,3:a->xyz\n
+    fn read_begin(&mut self) -> Result<Option<()>, WriterError> {
+        let mut buf: Vec<u8> = Vec::new();
 
-    let wrap = |err| { Error::IOError(err) };
+        // consume a '+'
+        let r = self.input.read_until(PLUS, &mut buf)?;
 
-    // consume a '+'
-    let r = input.read_until(PLUS, &mut buf).map_err(wrap)?;
+        assert_eq!(r, 1);
 
-    assert_eq!(r, 1);
-    assert_eq!(buf[0], PLUS);
-    buf.clear();
+        match buf[0] {
+            PLUS => Ok(Some(())),
+            NL => Ok(None),
+            wat => panic!("encountered unexpected char: {}", wat),
+        }
+    }
 
-    let r = input.read_until(COMMA, &mut buf).map_err(wrap)?;
+    fn read_sizes(&mut self) -> Result<KVSizes, WriterError> {
+        let mut buf: Vec<u8> = Vec::new();
 
-    assert!(r > 0);
-    buf.pop(); // trim the comma off the end
+        // consume a '+'
+        let r = self.input.read_until(PLUS, &mut buf)?;
 
-    let k = parse_digits(&buf)?;
-    buf.clear();
+        assert_eq!(r, 1);
+        assert_eq!(buf[0], PLUS);
+        buf.clear();
 
-    let r = input.read_until(COLON, &mut buf).map_err(wrap)?;
+        let r = self.input.read_until(COMMA, &mut buf)?;
 
-    assert!(r > 0);
-    assert_eq!(COLON, buf.pop().unwrap());
-    let v = parse_digits(&buf)?;
+        assert!(r > 0);
+        assert_eq!(COMMA, buf[buf.len()-1]);
+        buf.pop(); // trim the comma off the end
 
-    Ok(KVSizes(k, v))
-}
+        let k = parse_digits(&buf)?;
+        buf.clear();
 
-fn read_kv<R: Read>(input: &mut BufReader<R>, kvs: &KVSizes) -> Result<KV, Error> {
-    let KVSizes(ksize, vsize) = kvs;
+        let r = self.input.read_until(COLON, &mut buf)?;
 
-    let mut kbytes = Vec::with_capacity(*ksize);
-    input.read_exact(&mut kbytes).map_err(|e| Error::IOError(e))?;
+        assert!(r > 0);
+        assert_eq!(COLON, buf.pop().unwrap());
+        let v = parse_digits(&buf)?;
 
-    // consume the "->" between k and v
-    let mut arrowbytes: [u8; 2] = [0; 2];
-    input.read_exact(&mut arrowbytes).map_err()
+        Ok(KVSizes(k, v))
+    }
 
+    fn read_kv(&mut self, kvs: &KVSizes) -> Result<KV, WriterError> {
+        let KVSizes(ksize, vsize) = kvs;
 
-    let mut vbytes = Vec::with_capacity(*vsize);
-    input.read_exact(&mut vbytes).map_err(|e| Error::IOError(e))?;
+        let mut kbytes = Vec::with_capacity(*ksize);
+        self.input.read_exact(&mut kbytes)?;
 
-    Ok(KV{k: Bytes::from(kbytes), v: Bytes::from(vbytes)})
-}
+        // consume the "->" between k and v
+        let mut arrowbytes: [u8; 2] = [0; 2];
+        self.input.read_exact(&mut arrowbytes)?;
+        assert_eq!(arrowbytes, *ARROW_BYTES);
 
-fn read_one_record<R: Read>(input: &mut BufReader<R>) -> Result<KV, Error> {
-    Ok(KV{k: Bytes::new(), v: Bytes::new()})
+        let mut vbytes = Vec::with_capacity(*vsize);
+        self.input.read_exact(&mut vbytes)?;
+
+        Ok(KV { k: Bytes::from(kbytes), v: Bytes::from(vbytes) })
+    }
+
+    fn read_one_record(&mut self) -> Result<Option<KV>, WriterError> {
+        match self.read_begin()? {
+            None => Ok(None),
+            Some(_) =>
+                self.read_sizes()
+                    .and_then(|sizes| self.read_kv(&sizes))
+                    .map(|kv| Some(kv))
+        }
+    }
+
+    pub fn iter<'a>(&'a mut self) -> Box<Iterator<Item=Result<Option<KV>, WriterError>> + 'a> {
+        
+    }
+
+    pub fn new<T: Read>(buf: BufReader<T>) -> Parser<T> {
+        Parser{input: buf}
+    }
 }
 
 // expects input in
 pub fn write(input: &mut File) {
     let buf = BufReader::new(input);
-
-
 }
 
